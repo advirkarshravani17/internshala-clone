c03a0735a29103ba8b9358fd41be04e8997dab16
Shravani Advirkar <advirkarshravani17@gmail.com>
2026-03-06 17:29:24 +0530

    temporary rename

--- internarea/src/store/store.js
+++ internarea/src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../feature/Userslice";
+import userReducer from "../feature/Tempslice";
 export const store = configureStore({
   reducer: {
     user: userReducer,
